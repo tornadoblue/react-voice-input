2582cf2ec08b42cca6b53e0e6459dd8811687716
[dyad] <git@dyad.sh>
2025-06-15 15:47:13 -0700

    [dyad] Updated README.md with detailed prop information for styling and customization. - wrote 1 file(s) + extra files edited outside of Dyad

---
+++ src/index.js
@@ -0,0 +1,5 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.VoiceInputCapture = void 0;
+var VoiceInputCapture_1 = require("./components/VoiceInputCapture");
+Object.defineProperty(exports, "VoiceInputCapture", { enumerable: true, get: function () { return VoiceInputCapture_1.VoiceInputCapture; } });
